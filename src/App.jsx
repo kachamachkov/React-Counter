feebc7f8126f6a59681189bef1dc5421a4925ea6
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-13 11:32:21 +0300

    Add increment, decrement, reset functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <>
-      <div className='wrapper'>
+      <div className="wrapper">
         <h2>React Counter</h2>
-        <div className="display">Count</div>
-        <button>Increment</button>
-        <button>Decrement</button>
-        <button>Reset</button>
+        <div className="display">{counter}</div>
+        <button onClick={() => setCounter((c) => (c += 1))}>Increment</button>
+        <button onClick={() => setCounter((c) => (c -= 1))}>Decrement</button>
+        <button onClick={() => setCounter(0)}>Reset</button>
       </div>
     </>
   );
